76cb53b227ff2012f698f6ffca77f4f50157dd27
cdangeti <charandangeti7@gmail.com>
2024-08-14 14:11:03 -0400

    Update script.js login

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
 
-        // Check if user is an organization
+        // Determine if user is an organization
         const isOrganization = email.includes('.org') && email.includes('@');
         const organizations = JSON.parse(localStorage.getItem('organizations')) || [];
         const students = JSON.parse(localStorage.getItem('students')) || [];
